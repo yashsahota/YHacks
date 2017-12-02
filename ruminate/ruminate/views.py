67edf93c69ebac8bf5ea42db32f00b6d6090ab50
Yash Sahota <yashsahota@icloud.com>
2017-12-02 13:20:32 -0500

    Added past OH stuff

--- ruminate/ruminate/views.py
+++ ruminate/ruminate/views.py
@@ -15,13 +15,23 @@ def course(request):
     live_list = Office_Hour.objects.filter(is_live=True)
     class_list = User.objects.filter(name="Ryan Slama")[0].course_enrolled.all
 
-    old_list = Office_Hour.objects.filter(is_live=False).filter(end_time__gt=dt.now()) 
+    presortold_list = Office_Hour.objects.filter(is_live=False).filter(end_time__lt=dt.now()) 
+    old_list = []
+
+    for x in range(0,7):
+        end_time_calc = dt.now() - datetime.timedelta(days=x)
+        start_time_calc = end_time_calc - datetime.timedelta(days=1)
+        old_list.insert(x, list(presortold_list.filter(end_time__lt=(end_time_calc), end_time__gt=(start_time_calc))))
+
 
     print("Live list is ", live_list)
     print("Class list is ", class_list)
+    print("Preold list is", presortold_list)
+    print("Old list is ", old_list)
     context = {
         'live_list': live_list,
         'class_list': class_list,
+        'old_list': old_list,
     }
     template = loader.get_template('ruminate/app.html')
     return HttpResponse(template.render(context, request))
