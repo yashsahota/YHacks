8d6024e541ffb5ff510ca59a77def609d7eafa76
Ryan Slama <rms427@cornell.edu>
2017-12-02 03:33:46 -0500

    What I have now

--- ruminate/ruminate/views.py
+++ ruminate/ruminate/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 import datetime
 
 
 def course(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    # now = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+    # return HttpResponse(html)
+    return render(request, 'ruminate/app.html')
