61eed5e07c971f170b02d32b8d1791c129010b4c
Ryan Slama <rms427@cornell.edu>
2017-12-02 03:41:31 -0500

    Template working

--- ruminate/ruminate/settings.py
+++ ruminate/ruminate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ruminate.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
